91c7bed7e0c40ccfccadcc3d30d02a24a0a7182d
DUEM <electric.motorsport@dur.ac.uk>
2016-06-15 18:18:11 +0000

    initial run script

---
+++ CANlog.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# enable can0
+sudo ip link set can0 type can bitrate 1000000
+sudo ip link set can0 up
+
+# activate venv
+source /root/Datalogger_Scripts/.venv/bin/activate
+
+# run CANlog.py
+python3 /root/Datalogger_Scripts/CANlog.py
